1d7c2e3138451d6b12ae813e0b6b4543cfb69012
Jan Berges <jan.berges@uni-bremen.de>
2015-03-25 19:57:36 +0100

    Wrapped the intrinsic subroutine GET_COMMAND_ARGUMENT with a function

--- io.f90
+++ io.f90
@@ -2,14 +2,14 @@ module io
    implicit none
    private
 
-   public :: slurp
+   public :: slurp, command_argument
 
 contains
 
    function slurp(file) result(content)
-      character(*), intent(in) :: file
-
       character(:), allocatable :: content
+
+      character(*), intent(in) :: file
 
       integer, parameter :: unit = 16
       integer :: size
@@ -29,4 +29,18 @@ contains
 
       close(unit)
    end function slurp
+
+   function command_argument(i) result(argument)
+      character(:), allocatable :: argument
+
+      integer, intent(in) :: i
+
+      integer :: size
+
+      call get_command_argument(i, length=size)
+
+      allocate(character(size) :: argument)
+
+      call get_command_argument(i, value=argument)
+   end function command_argument
 end module io
--- main.f90
+++ main.f90
@@ -1,28 +1,28 @@
 program tonbandfetzen
    use aiff
+   use io
    use rationals
    use search
    implicit none
 
    type(audio) :: s
 
-   integer :: i, l
-   character(20) :: x
-   character(:), allocatable :: n
+   integer :: i
+   character(:), allocatable :: x, n
 
    do i = 1, command_argument_count()
-      call get_command_argument(i, x, l)
+      x = command_argument(i)
 
-      select case(x(l - 3:l))
+      select case(x(len(x) - 3:))
          case ('.aif')
-            call take(x(:l), s)
+            call take(x, s)
 
             s%sound(:, :) = s%sound(:, s%points:1:-1)
 
-            call make(x(:l - 4) // '_reversed' // x(l - 3:l), s)
+            call make(x(:len(x) - 4) // '_reversed' // x(len(x) - 3:), s)
 
          case ('.txt')
-            call focus(x(:l))
+            call focus(x)
 
             do
                n = next('0123456789.:')

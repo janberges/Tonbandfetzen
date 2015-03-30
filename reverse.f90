de48e11514ab4ee803d6cbf6157102132082d111
Jan Berges <jan.berges@uni-bremen.de>
2015-03-30 21:48:58 +0200

    Added a function which parses paths and returns them without suffix

--- reverse.f90
+++ reverse.f90
@@ -1,23 +1,20 @@
 program reverse
    use aiff
+   use paths
    use io
    implicit none
 
-   integer :: i, dot
-   character(:), allocatable :: file
+   integer :: i
+   character(:), allocatable :: path
    type(audio) :: s
 
    do i = 1, command_argument_count()
-      file = command_argument(i)
+      path = command_argument(i)
 
-      call take(file, s)
+      call take(path, s)
 
       s%sound(:, :) = s%sound(:, s%points:1:-1)
 
-      dot = index(file, '.', back=.true.)
-
-      if (dot .eq. 0) dot = len(file) + 1
-
-      call make(file(:dot - 1) // '_reversed' // file(dot:), s)
+      call make(stem(path) // '_reversed.aif', s)
    end do
 end program reverse

898c6f55fdb810271c66c65adf0ed5fe4966a177
Jan Berges <jan.berges@uni-bremen.de>
2020-10-03 13:11:21 +0200

    Repeat data an integer number of times

---
+++ repeat.f90
@@ -0,0 +1,31 @@
+program repeat
+   use aiff
+   use paths
+   use io
+   implicit none
+
+   integer :: i
+   character(:), allocatable :: path, n
+   type(audio) :: s1
+   type(audio) :: s
+
+   path = command_argument(1)
+   n    = command_argument(2)
+
+   call take(path, s1)
+
+   read (n, *) s%points
+
+   s%channels  = s1%channels
+   s%points    = s1%points * s%points
+   s%rate      = s1%rate
+   s%amplitude = s1%amplitude
+
+   allocate(s%sound(s%channels, s%points))
+
+   do i = 1, s%points, s1%points
+      s%sound(:, i:i + s1%points) = s1%sound
+   end do
+
+   call make(stem(path) // 'x' // n // '.aif', s)
+end program repeat

b06468c9039f13306a96ec506c3ac94a305d08ef
Jan Berges <jan.berges@uni-bremen.de>
2020-10-03 18:26:46 +0200

    Implement sound effect "flanger"

---
+++ src/flange.f90
@@ -0,0 +1,34 @@
+program flange
+   use constants
+   use aiff
+   use paths
+   use io
+   implicit none
+
+   integer :: i
+   character(:), allocatable :: path, ampl
+   real(dp) :: amplitude
+   type(audio) :: s1
+   type(audio) :: s
+
+   path = command_argument(1)
+   ampl = command_argument(2)
+
+   call take(path, s1)
+
+   read (ampl, *) amplitude
+
+   s%channels  = s1%channels
+   s%points    = s1%points
+   s%rate      = s1%rate
+   s%amplitude = s1%amplitude
+
+   allocate(s%sound(s%channels, s%points))
+
+   do i = 0, s%points - 1
+      s%sound(:, i) = s1%sound(:, i &
+         + nint(amplitude * s%rate * sin(2 * pi * i / (s%points - 1)) ** 2))
+   end do
+
+   call make(stem(path) // '_flanged.aif', s)
+end program flange

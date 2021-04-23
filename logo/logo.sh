6020e1169de75d871bf760613b62b7be4ff10a7d
Jan Berges <jan.berges@uni-bremen.de>
2021-04-23 20:08:20 +0200

    Add padding to logo banner

--- logo/logo.sh
+++ logo/logo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 a=20
-px=640
 in=2.54
 
 pdflatex --interaction=batchmode logo
 
-convert -density `perl -e "print $px / ($a / $in)"` logo.pdf \
+convert -density `perl -e "print 640 / ($a / $in)"` logo.pdf \
     -flatten PNG8:logo.png
 
-convert -density `perl -e "print $px / ($a / $in)"` logo.pdf \
+convert -density `perl -e "print 480 / ($a / $in)"` logo.pdf \
     -background white -gravity center -extent 1280x640 PNG8:logo_banner.png

736c2a819e5cf958b0bac9bb1e9fc4706b5015b4
Jan Berges <jan.berges@uni-bremen.de>
2021-08-06 13:23:19 +0200

    Add example with downwards spiral of fifths

---
+++ examples/spiral/spiral.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+letters = 'FCGDAEB'
+signs = ['bbb', 'bb', 'b', '', '#', 'x', 'x#']
+notes = [letter + sign for sign in signs for letter in letters]
+notes = notes[::-1]
+
+tracks = ['[10', '[6', '[2', ']2', ']6', ']10']
+sounds = [(2, 'cubic'), (3, 'major')]
+
+print('Tpyth')
+print('|%d' % len(tracks))
+
+print('M')
+for octave, wave in sounds:
+    print('~%s' % wave)
+    for n, track in enumerate(tracks):
+        print("W '%d:%d %s" % (n, len(tracks), track))
+        for note in notes[n::len(tracks)]:
+            print("%s%d <20''>20'`" % (note, octave))

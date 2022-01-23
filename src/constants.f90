5c7db99711a737ebe7243c8eba5743b807b37375
Jan Berges <jan.berges@uni-bremen.de>
2022-01-23 11:47:21 +0100

    Get double-precision kind from ISO_FORTRAN_ENV

--- src/constants.f90
+++ src/constants.f90
@@ -4,15 +4,13 @@ module constants
       i2 => int16, &
       i4 => int32, &
       i8 => int64, &
+      dp => real64, &
       eof => iostat_end, &
       eol => iostat_eor, &
       stderr => error_unit
    implicit none
-   private
+   public
 
-   public :: audio, dp, eof, eol, i1, i2, i2max, i4, i8, pi, stderr
-
-   integer, parameter :: dp = selected_real_kind(14, 300)
    integer, parameter :: i2max = 2 ** 15 - 1
 
    real(dp), parameter :: pi = 4.0_dp * atan(1.0_dp)

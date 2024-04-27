8e23da6c924ccfef0f5b0b2098de7e18e88c408d
Jan Berges <jan.berges@uni-bremen.de>
2024-04-27 22:16:58 +0200

    Remove unused variable

--- src/mel.f90
+++ src/mel.f90
@@ -5,10 +5,7 @@ program mel
    use riff, only: write_riff
    implicit none
 
-   integer :: n
    type(audio) :: music
-
-   n = command_argument_count()
 
    call play(slurp(command_argument(-2, '/dev/stdin')), music)
    call write_riff(command_argument(-1, '/dev/stdout'), music)

56830b717f58c564f333163a2a53cc75da5c65bd
Jan Berges <jan.berges@uni-bremen.de>
2024-12-07 20:16:22 +0100

    Pluck strings in CGI example

--- src/cgi.f90
+++ src/cgi.f90
@@ -110,8 +110,11 @@ program mel_dot_cgi
          "$22050", &
          "*Harmonic series*", &
          "T pyth", &
+         "X synth", &
+         '"', &
          "M A2'8", &
          "W ,5 A2' A3' E4' A4' C#v5' E5' Gz5' A5'", &
+         '"', &
          "</textarea>", &
          "    <button id='play' onclick='play()'>Interpret</button>", &
          "    <audio id='wav' controls autoplay>Sorry</audio>", &

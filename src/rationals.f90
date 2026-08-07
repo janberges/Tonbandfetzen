41e1d6cbedffc504a992e625e4412f22f1c6ca02
Jan Berges <jan.berges@uni-bremen.de>
2026-08-07 18:56:35 +0200

    Change default numerator

--- src/rationals.f90
+++ src/rationals.f90
@@ -22,7 +22,7 @@ contains
          if (error .ne. 0) rational = 0.0_dp
       else
          read (ratio(:i - 1), *, iostat=error) numerator
-         if (error .ne. 0) numerator = 0.0_dp
+         if (error .ne. 0) numerator = 1.0_dp
 
          read (ratio(i + 1:), *, iostat=error) denominator
          if (error .ne. 0) denominator = 1.0_dp

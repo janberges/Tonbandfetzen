31eda4504f0d88d76f0bd7a747d7e915df849d01
Jan Berges <jan.berges@uni-bremen.de>
2026-08-09 15:11:18 +0200

    Adapt play wrapper to macOS

--- src/play.f90
+++ src/play.f90
@@ -2,21 +2,29 @@ subroutine playz
    use aiff, only: read_aiff
    use constants, only: audio
    use interpreter, only: play
-   use io, only: command_argument, slurp
+   use io, only: command_argument, environment_variable, slurp
    use paths, only: extension
    use riff, only: write_riff
    use tab, only: preprocess
    implicit none
 
-   character(:), allocatable :: infile, outfile
+   character(:), allocatable :: infile, outfile, command
    type(audio) :: s
 
+   outfile = 'tz-play.tmp.wav'
+   command = 'open'
+
+   if (index(environment_variable('OSTYPE'), 'darwin') .eq. 0) then
+      outfile = '/dev/shm/' // outfile
+      command = 'xdg-' // command
+   end if
+
    infile = command_argument(1, '/dev/stdin')
-   outfile = command_argument(2, '/dev/shm/tmp.wav')
+   outfile = command_argument(2, outfile)
 
    select case (extension(infile))
    case ('wave', 'wav', 'mp3', 'WAVE', 'WAV', 'MP3')
-      call execute_command_line('xdg-open ' // infile)
+      call execute_command_line(command // ' ' // infile)
       return
 
    case ('aiff', 'aif', 'AIFF', 'AIF')
@@ -27,5 +35,5 @@ subroutine playz
    end select
 
    call write_riff(outfile, s)
-   call execute_command_line('xdg-open ' // outfile)
+   call execute_command_line(command // ' ' // outfile)
 end subroutine playz
